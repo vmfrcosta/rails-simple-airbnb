f4a6f8a6cb74cb448cf62bec69347c3f4ba692c7
vinicius <vmfrcosta@gmail.com>
2019-11-13 17:34:06 -0300

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :flats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'flats#index'
+  # get 'flats', to: 'flats#index', as: 'flats'
+  # get 'flats/new', to: 'flats#new', as: 'new_flat'
+  # get 'flats/:id', to: 'flats#show', as: 'flat'
+  # post 'flats', to: 'flats#create'
+  # get 'flats/:id/edit', to: 'flats#edit', as: 'edit_flat'
+  # patch 'flats/:id', to: 'flats#update'
+  # delete 'flats/:id', to: 'flats#destroy'
 end
